2943ef0b9e74e1b6181bd154bd7b22148370ded8
whitemouse25 <whitemouse25@users.noreply.github.com>
2025-01-20 22:36:30 +0000

    deploy: 809f63cdd14a6aeef68da1ce54a2e6c7858f09c4

---
+++ app.js
@@ -0,0 +1,21 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+// Add Task
+addTaskBtn.addEventListener('click', () => {
+    const task = taskInput.value.trim();
+    if (task) {
+        const li = document.createElement('li');
+        li.textContent = task;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+});
+
+// Remove Task on Click
+taskList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        e.target.remove();
+    }
+});
